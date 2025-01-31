e2cf288ab377270c43995635e27740dc95f3cad4
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-01-31 00:48:37 -0800

    Uzum

---
+++ Uzum/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from django.views.generic import CreateView
+from django.views.generic import UpdateView
+from django.views.generic import DeleteView
+from django.views.generic import View
+
+def index(request):
+    return render(request, 'index.html')
+
+def home(request):
+    return render(request, 'home.html')
+def navigation(request):
+    return render(request, 'navigation.html')
