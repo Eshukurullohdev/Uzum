83433189cd41370fbc0c60875eef975b65f1a860
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-02-04 22:05:34 -0800

    GmCar

--- Uzum/urls.py
+++ Uzum/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('home/', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('navigation/', views.navigation, name='navigation'),
     path('banner/', views.banner, name='banner'),
 ]
