241baf476afda90ad73e91c49c12faafe2877e42
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-02-09 02:51:27 -0800

    add login and messaga

--- Authentication/views.py
+++ Authentication/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as auth_login  # loginni boshqa nom bilan import qilish
+from django.contrib.auth import logout as auth_logout
+
 from django.contrib import messages
 
 def login(request):
@@ -11,10 +13,16 @@ def login(request):
 
         if user is not None:
             auth_login(request, user) 
-            print('ohshadi')# to'g'ri login chaqiruv
+            messages.success(request, "Siz Login Sahifaziga keldingiz!")
             return redirect('home')
         else:
             messages.error(request, 'Login yoki parol noto‘g‘ri!')
             return redirect('login')
 
     return render(request, 'login.html')
+
+
+def logout(request):
+    auth_logout(request)  # Foydalanuvchini sessiyadan chiqarish
+    messages.success(request, "Siz Tizimdan muvaffaqiyatli chiqib ketdingiz!")
+    return render(request, 'logout.html')
--- Uzum/views.py
+++ Uzum/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .models import Uzum
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-
-
+@login_required(login_url='login')
 def home(request):
     uzum = Uzum.objects.all()
+    messages.success(request, 'Siz Tizimga muvaffaqiyatli kirdingiz!')
     return render(request, 'home.html' , {'uzums': uzum})
 
 
