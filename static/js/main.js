8bd6656a7b1b176c513f28ec4973d523d9d893f0
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-02-13 08:38:49 -0800

    add tovar carusel

--- static/js/main.js
+++ static/js/main.js
@@ -20,3 +20,17 @@ document.addEventListener('DOMContentLoaded', function () {
       updateCarousel();
     });
   });
+
+
+
+
+  $(document).ready(function(){
+    $('.carousel').slick({
+        vertical: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false
+    });
+});
